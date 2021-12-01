67e6c4d2804c1391165ed4c91be1660647585327
sarahanngwynn <93155807+sarahanngwynn@users.noreply.github.com>
2021-12-01 12:29:08 -0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Sarah Gwynn and is{" "}
+        <a href="https://fervent-benz-a17bf6.netlify.app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
