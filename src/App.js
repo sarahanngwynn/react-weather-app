620f28e4fe0d17124e87bff03c54190b1b4f5694
sarahanngwynn <93155807+sarahanngwynn@users.noreply.github.com>
2021-12-01 12:31:20 -0700

    updated open source

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         This project is created by Sarah Gwynn and is{" "}
-        <a href="https://fervent-benz-a17bf6.netlify.app" target="_blank">
+        <a href="https://fervent-benz-a17bf6.netlify.app">
           open-sourced on GitHub
         </a>
       </footer>
