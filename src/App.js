82c0f47c509ea8370cf41c98e6b1d5b047861f46
sarahanngwynn <93155807+sarahanngwynn@users.noreply.github.com>
2021-12-01 14:38:09 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <button class="btn btn-primary">Hello</button>
+        <Weather />
         <footer>
           This project is created by Sarah Gwynn and is{" "}
           <a href="https://fervent-benz-a17bf6.netlify.app">
---
+++ src/Weather.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="w-100 btn btn-primary"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wedenesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
+              className="float-left"
+            />
+
+            <span className="temperature">49</span>
+            <span className="unit">°F</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 48%</li>
+            <li>Wind: 2mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
