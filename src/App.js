54c349adbd6afe057d5a153917a38ec70545a72a
sarahanngwynn <93155807+sarahanngwynn@users.noreply.github.com>
2021-12-06 18:43:10 -0700

    fixed icon alignment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
@@ -17,5 +17,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
